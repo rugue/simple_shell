838da4713179533cf2223452e0be7fea065aa8b0
Enehizena Osarugue <nehirugue@gmail.com>
2023-05-19 14:18:23 +0100

    Created errors1.c

---
+++ errors1.c
@@ -0,0 +1,138 @@
+#include "shell.h"
+
+/**
+ * _erratoi - this convert str to an int
+ * @s: the str to be convertd
+ * Return: 0 if no numbs in str, convertd numb otherwise -1
+ */
+int _erratoi(char *s)
+{
+	int a = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;
+	for (a = 0;  s[a] != '\0'; a++)
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			result *= 10;
+			result += (s[a] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - prints error message
+ * @info: paramet & retn info struct
+ * @estr: str contains specified error-type
+ * Return: 0 if no numb str, convert numb otherwise -1
+ */
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+
+/**
+ * print_d - functn print deci number
+ * @input: this is the input
+ * @fd: the FileDescriptor to write into
+ *
+ * Return: numb of characters print
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int a, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (a = 1000000000; a > 1; a /= 10)
+	{
+		if (_abs_ / a)
+		{
+			__putchar('0' + current / a);
+			count++;
+		}
+		current %= a;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - convert functn, clone of itoa
+ * @num: numbers
+ * @base: the base
+ * @flags: the argument flags
+ *
+ * Return: strings
+ */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * remove_comments - helps replaces 1st instance of '#' with '\0'
+ * @buf: adress of str to modify
+ *
+ * Return: always return zero0;
+ */
+void remove_comments(char *buf)
+{
+	int a;
+
+	for (a = 0; buf[a] != '\0'; a++)
+		if (buf[a] == '#' && (!a || buf[a - 1] == ' '))
+		{
+			buf[a] = '\0';
+			break;
+		}
+}
