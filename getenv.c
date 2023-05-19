afc88e87e2bd773815b528c7d48c7066266753a0
Enehizena Osarugue <nehirugue@gmail.com>
2023-05-19 14:19:20 +0100

    Created getenv.c

---
+++ getenv.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * get_environ - this returns the str array copy of environ
+ * @info: structure contains potential args for Used to maintaining
+ *          const functn prototype
+ * Return: always return zero0
+ */
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - this Remove an environment var
+ * @info: Struct contains potential args. for maintain
+ *        const functn prototype
+ *  Return: 1 for delete, 0 if not
+ * @var: the str env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - this Initialize new environment var, or modify existing 1
+ * @info: Structure containing potential arguments. Used to maintain
+ * @var: the str env var propert
+ * @value: the str env var value
+ *  Return: always return 0
+ */
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
