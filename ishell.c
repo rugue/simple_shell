b26e52ecdfbc084f98dc1103b8175a98c5404df9
Osarugue Enehizena <nehirugue@gmail.com>
2023-10-20 18:56:39 -0700

    Authors

--- ishell.c
+++ ishell.c
@@ -1,23 +1,23 @@
-#include "ishell.h"
+#include "shell.h"
 /**
- * main - Entry point for the shell program
- * @argc: Number of arguments from the command line
- * @argv: Array of command line arguments
- * @env: Environment variables
- * Return: 0 on success
+ * main - initialize the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on succes.
  */
 int main(int argc, char *argv[], char *env[])
 {
 	data_of_program data_struct = {NULL}, *data = &data_struct;
 	char *prompt = "";
 
-	initialize_data(data, argc, argv, env);
+	inicialize_data(data, argc, argv, env);
 
 	signal(SIGINT, handle_ctrl_c);
 
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
-	{
-		errno = 2;
+	{/* We are in the terminal, interactive mode */
+		errno = 2;/*???????*/
 		prompt = PROMPT_MSG;
 	}
 	errno = 0;
@@ -25,12 +25,10 @@ int main(int argc, char *argv[], char *env[])
 	return (0);
 }
 
-
-
 /**
- * handle_ctrl_c - this function prints a new line and the prompt
- * when the program receives a SIGINT signal (ctrl + c)
- * @UNUSED: option of prototype
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
  */
 void handle_ctrl_c(int opr UNUSED)
 {
@@ -38,23 +36,22 @@ void handle_ctrl_c(int opr UNUSED)
 	_print(PROMPT_MSG);
 }
 
-
 /**
- * initialize_data - set up struct with the program information
- * @data: pointer to data structure
- * @argv: array of arguments passed to program execution
- * @env: environment variables passed to program execution
- * @argc: number of values received from command line
+ * inicialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
  */
-void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
-	int j = 0;
+	int i = 0;
 
 	data->program_name = argv[0];
 	data->input_line = NULL;
 	data->command_name = NULL;
 	data->exec_counter = 0;
-
+	/* define the file descriptor to be readed*/
 	if (argc == 1)
 		data->file_descriptor = STDIN_FILENO;
 	else
@@ -73,49 +70,49 @@ void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
 	data->env = malloc(sizeof(char *) * 50);
 	if (env)
 	{
-		for (; env[j]; j++)
+		for (; env[i]; i++)
 		{
-			data->env[j] = str_duplicate(env[j]);
+			data->env[i] = str_duplicate(env[i]);
 		}
 	}
-	data->env[j] = NULL;
+	data->env[i] = NULL;
 	env = data->env;
 
 	data->alias_list = malloc(sizeof(char *) * 20);
-	for (j = 0; j < 20; j++)
+	for (i = 0; i < 20; i++)
 	{
-		data->alias_list[j] = NULL;
+		data->alias_list[i] = NULL;
 	}
 }
 /**
- * sisifo - it's an infinite loop that shows prompt
- * @prompt: prompt for printing
- * @data: it's an infinite loop that shows prompt
+ * sisifo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
  */
 void sisifo(char *prompt, data_of_program *data)
 {
-	int status = 0, length = 0;
+	int error_code = 0, string_len = 0;
 
 	while (++(data->exec_counter))
 	{
 		_print(prompt);
-			status = length = _getline(data);
+		error_code = string_len = _getline(data);
 
-		if (status == EOF)
+		if (error_code == EOF)
 		{
 			free_all_data(data);
-			exit(errno);
+			exit(errno); /* if EOF is the fisrt Char of string, exit*/
 		}
-		if (length >= 1)
+		if (string_len >= 1)
 		{
-			resolve_alias(data);
-			resolve_variables(data);
-			split(data);
+			expand_alias(data);
+			expand_variables(data);
+			tokenize(data);
 			if (data->tokens[0])
-			{
-				status = run(data);
-				if (status != 0)
-					_print_error(status, data);
+			{ /* if a text is given to prompt, execute */
+				error_code = execute(data);
+				if (error_code != 0)
+					_print_error(error_code, data);
 			}
 			free_recurrent_data(data);
 		}
